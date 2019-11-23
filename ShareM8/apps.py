7bafafa3b2c5bb46ec53b72ddd51d3852935c52e
Paun Tudor <tudorpaun91@yahoo.com>
2019-11-23 13:46:20 +0200

    django project added

---
+++ ShareM8/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Sharem8Config(AppConfig):
+    name = 'ShareM8'
---
+++ ShareM8DB/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ShareM8DB project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShareM8DB.settings')
+
+application = get_wsgi_application()
